7faacaff1541f943148b3979b4ee29cdcd84e797
SmartDever02 <smart.jamesjin@gmail.com>
2022-07-24 15:24:53 -0400

    change heading world->tracker

--- src/views/Landing/Landing.tsx
+++ src/views/Landing/Landing.tsx
@@ -5,7 +5,7 @@ const Landing = () => {
     <div className='flex justify-center items-center w-screen h-screen bg-cover bg-no-repeat bg-[-center_top_2rem] bg-[url("assets/background/EOS-Network.jpeg")]'>
       <div className='flex items-center flex-col gap-8'>
         <h1 className='text-[60px] text-white font-bold'>
-          Welcome To EOS World
+          Welcome To EOS Tracker
         </h1>
         <p className='text-white text-10'>
           EOSIO is an open-source blockchain platform that helps developers,
